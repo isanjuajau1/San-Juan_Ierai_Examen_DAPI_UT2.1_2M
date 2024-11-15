2e3423799dca991e4ff322ed6dfe47d5e3fcd68c
isanjuajau1 <isanjuajau1@educacion.navarra.es>
2024-11-15 09:38:37 +0100

    EJERCICIO 1

---
+++ Ejercicio 1.py
@@ -0,0 +1,4 @@
+comida = input("dime tu comida favorita\n" )
+print( f"me encanta {comida} a mi tambien" )
+edad= input ("Que edad tienes ?:\n") 
+edad = int( 12)
---
+++ Ejercicio 3.py
@@ -0,0 +1,10 @@
+numero0 = int(input("Nº y apellido porfavor:"))
+if numero0 % 2 == 0:
+    print(numero0, "es par")
+else:
+    print(numero0, "es inpar")
+numero1 = int(input("Dime un Nº: "))
+for i in range(1, numero1 + 1):
+    if i % 3 == 0:
+        list.append=print("dime un Nº enetro ")
+print("muestra lista")
